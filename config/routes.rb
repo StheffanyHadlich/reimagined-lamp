a95a4187527538fe2214a24a26721ae6097d97d7
Stheffany <stheffanyhadlich@gmail.com>
2019-05-11 21:04:58 -0300

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  get 'users' => 'users#index'
-  get 'users/new' => 'users#new', as: :new_user
-  get 'users/:id/edit' => 'users#edit', as: :edit_user
-  get 'users/:id' => 'users#show', as: :user
-  post 'users' => 'users#create'
-  put 'users/:id' => 'users#update'
-  patch 'users/:id' => 'users#update'
-  delete 'users/:id' => 'users#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
